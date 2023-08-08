e0e0399826c29bd98fccfba88188c3cb251b6c57
Matheus <matheus_alvesrs13@hotmail.com>
2023-08-08 10:59:24 -0300

    att

---
+++ aula_85_list_compreenhesion+umFor/aula85.py
@@ -0,0 +1,16 @@
+
+lista = []
+# for x in range(3):
+
+#     for y in range(3):
+
+#         lista.append((x,y))
+lista = [
+    (x,y) for x in range(5) 
+    for y in range(3) 
+]
+
+
+
+lista = [[letra for letra in 'Edu']for x in range(3)]
+print(lista)
