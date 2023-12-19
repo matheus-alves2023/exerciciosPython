032a0367c28ab5876285574a958832932c9e791a
Matheus <matheus_alvesrs13@hotmail.com>
2023-12-19 19:40:36 -0300

    estavel

--- aula_77  - ExerciciosistemaPerguntaResposta/sistemaPerguntaresposta/questions_user.py
+++ aula_77  - ExerciciosistemaPerguntaResposta/sistemaPerguntaresposta/questions_user.py
@@ -1,21 +1,25 @@
 from os import system
 from time import sleep
+import string
 
 perguntas = [
     {
         'Pergunta': 'Quanto é 2+2?',
         'Opções': ['1', '3', '4', '5'],
-        'Resposta': '4',
+        'Resposta_letra': 'cC',
+        'Resposta_exata':'4'
     },
     {
         'Pergunta': 'Quanto é 5*5?',
         'Opções': ['25', '55', '10', '51'],
-        'Resposta': '25',
+        'Resposta_letra': 'aA',
+        'Resposta_exata':'25',
     },
     {
         'Pergunta': 'Quanto é 10/2?',
         'Opções': ['4', '5', '2', '1'],
-        'Resposta': '5',
+        'Resposta_letra': 'bB',
+        'Resposta_exata':'5'
     },
 ]
 
@@ -28,13 +32,18 @@ def front_quiz(SetQuestions):
         question = v.get('Pergunta')
         print(question)
         options = v.get('Opções')
-        for o in options:
-            print(o)
+        alternative_answer = v.get('Resposta_letra')
+        exact_answer = v.get('Resposta_exata')
+        letters = string.ascii_lowercase
+        for letter, o in zip(options,letters):
+            print(f'{o} - {letter} ')
+        
+                
         # print(options)
         user_option_input = input('Digite a opcao correta:')
-        answer = v.get('Resposta')
         
-        if user_option_input in  answer:
+        
+        if user_option_input in  alternative_answer or user_option_input in  exact_answer:
             hits += 1
             system('clear')
             print('Certa Resposta!')
@@ -49,6 +58,8 @@ def front_quiz(SetQuestions):
                f'question_log_{k}':  question,
                f'options_log_{k}': options,
                f'user_option_input_log_{k}': user_option_input,
+               f'exact_answer_log{k}': exact_answer,
+               f'alternative_answer_log{k}': alternative_answer,
                 
 
             }
@@ -58,6 +69,8 @@ def front_quiz(SetQuestions):
         'misses': misses,
     })
     print(f'Voce acertou {hits} de {hits + misses} perguntas.')
+    sleep(2)
+    system('clear')
     return data_question
 
-front_quiz(perguntas)
+print(front_quiz(perguntas))
--- aula_77  - ExerciciosistemaPerguntaResposta/sistemaPerguntaresposta/teste.py
+++
@@ -1,3 +0,0 @@
-lista = ['Renato','Marcelo']
-
-print('Renato' in lista)
