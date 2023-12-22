a5c7b002472c5e368e7948d39c891d6404d3f640
Matheus <matheus_alvesrs13@hotmail.com>
2023-12-22 17:36:27 -0300

    cadastroFInalizado

--- aula_77  - ExerciciosistemaPerguntaResposta/sistemaPerguntaresposta/libraries.py
+++ aula_77  - ExerciciosistemaPerguntaResposta/sistemaPerguntaresposta/libraries.py
@@ -1,5 +1,7 @@
 from os import system
 from time import sleep
+from questions_user import front_quiz
+from questions_register import cadastropergunta
 import string
 
 
--- aula_77  - ExerciciosistemaPerguntaResposta/sistemaPerguntaresposta/main.py
+++ aula_77  - ExerciciosistemaPerguntaResposta/sistemaPerguntaresposta/main.py
@@ -1,6 +1,28 @@
-from questions_user import front_quiz
-from questions_register import cadastropergunta
+
 import libraries
 
-front_quiz(cadastropergunta())
+
+
+def menu():
+    questionario_registrado = ''
+    while True:
+        libraries.sleep(1)
+        libraries.system("clear")
+        option_login = input(f'''Voce gostaria de acessar o modo admnistrador para registrar perguntas ou acessar o modo Aluno? 
+            [1] Administrador {len(questionario_registrado)} questionario(s) registrado.
+            [2] Aluno
+            Informe: ''')
+        if option_login in '1':
+            questionario_registrado = libraries.cadastropergunta()
+        elif option_login in '2' and questionario_registrado:
+            libraries.front_quiz(questionario_registrado)
+            questionario_registrado = ''
+        elif option_login in '2':
+            libraries.system('clear')
+            print("Nao ha nenhum questionario para responder!")
+        else:
+            libraries.system("clear")
+            print("Por favor, escolha uma opcao valida!")
+menu()
+# libraries.front_quiz(libraries.cadastropergunta())
 
